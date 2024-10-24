7f949188bcd5790ceb38cc1049f02203de0055e0
FelipeMS05 <fe_marcossi@yahoo.com.br>
2024-10-23 22:06:32 -0300

    Commit

---
+++ DesignPattern/Factory/LiuKang.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class LiuKang : PersonagemBase
+    {
+        public override void Escolhido()
+        {
+            Type name = this.GetType();
+            Console.WriteLine(GetName());
+        }
+
+        public override void Atacar()
+        {
+            Console.WriteLine("Ataque" + GetName());
+        }
+    }
+}
